cf850fdc6ef856b5fbdcad94a6f3f50a3edd9a0c
Dr. Sadi Vural <157088849+drsadivural@users.noreply.github.com>
2024-02-13 15:09:37 +1100

    Update About.svelte

--- src/lib/components/chat/Settings/About.svelte
+++ src/lib/components/chat/Settings/About.svelte
@@ -15,7 +15,7 @@
 <div class="flex flex-col h-full justify-between space-y-3 text-sm mb-6">
 	<div class=" space-y-3">
 		<div>
-			<div class=" mb-2.5 text-sm font-medium">{WEBUI_NAME} Version</div>
+			<div class=" mb-2.5 text-sm font-medium">{WEBUI_NAME} app version</div>
 			<div class="flex w-full">
 				<div class="flex-1 text-xs text-gray-700 dark:text-gray-200">
 					{$config && $config.version ? $config.version : WEB_UI_VERSION}
@@ -26,7 +26,7 @@
 		<hr class=" dark:border-gray-700" />
 
 		<div>
-			<div class=" mb-2.5 text-sm font-medium">AI-MED Version</div>
+			<div class=" mb-2.5 text-sm font-medium">AI-MED LLM version</div>
 			<div class="flex w-full">
 				<div class="flex-1 text-xs text-gray-700 dark:text-gray-200">
 					{ollamaVersion ?? 'N/A'}
@@ -40,23 +40,17 @@
 			<a href="https://discord.gg/5rJgQTnV4s" target="_blank">
 				<img
 					alt="Ayonix Pty ltd"
-					src="https://ayonix.com/wp-content/uploads/2022/08/Ayonix-Logo-HD-Beyaz.png"
+					src="https://img.shields.io/badge/Linkedin-Ayonix%20AI%20MED-blue?logo=linkedin&logoColor=white"
 				/>
 			</a>
 
-			<a href="https://au.linkedin.com/company/ayonix-inc." target="_blank">
-				<img
-					alt="Ayonix Linkedin"
-					src="https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo.svg"
-				/>
-			</a>
 		</div>
 
 		<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">
 			Developed by <a
 				class=" text-gray-500 dark:text-gray-300 font-medium"
 				href="https://ayonix.com"
-				target="_blank">Ayonix Pty Ltd, AI Center, VIC/Australia</a
+				target="_blank">Ayonix Pty Ltd, AI Center, Australia</a
 			>
 		</div>
 	</div>
