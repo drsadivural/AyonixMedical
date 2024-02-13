5d2fb437f05c94705e904b41434d1e06b5ff3aec
Dr. Sadi Vural <157088849+drsadivural@users.noreply.github.com>
2024-02-13 15:51:50 +1100

    Update +page.svelte

--- src/routes/(app)/prompts/+page.svelte
+++ src/routes/(app)/prompts/+page.svelte
@@ -12,9 +12,9 @@
 	let query = '';
 
 	const sharePrompt = async (prompt) => {
-		toast.success('Redirecting you to OllamaHub');
+		toast.success('Redirecting you to Ayonix AI-MED Cloud');
 
-		const url = 'https://ollamahub.com';
+		const url = 'http://gpt.ayonix.com';
 
 		const tab = await window.open(`${url}/prompts/create`, '_blank');
 		window.addEventListener(
@@ -272,11 +272,11 @@
 			</div>
 
 			<div class=" my-16">
-				<div class=" text-2xl font-semibold mb-6">Made by OllamaHub Community</div>
+				<div class=" text-2xl font-semibold mb-6">Made by Ayonix AI-MED Community</div>
 
 				<a
 					class=" flex space-x-4 cursor-pointer w-full mb-3"
-					href="https://ollamahub.com/?type=prompts"
+					href="http://gpt.ayonix.com"
 					target="_blank"
 				>
 					<div class=" self-center w-10">
